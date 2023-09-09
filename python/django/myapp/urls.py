ceb0d58c1cfaa0cf147bbd27ad36f7a0d5970c83
dotori1091@gmail.com <dotori1091@gmail.com>
2023-09-09 18:46:31 +0900

    django tutorial Fin.

--- python/django/myapp/urls.py
+++ python/django/myapp/urls.py
@@ -5,5 +5,7 @@ from myapp import views
 urlpatterns = [
     path('', views.index), # ''로 들어오면 views의 index로 접근
     path('create/', views.create),
-    path('read/<id>/', views.read) #<name>으로 가변 값 지정 가능
+    path('read/<id>/', views.read), #<'이름'>으로 가변 값 지정 가능
+    path('update/<id>/', views.update),
+    path('delete/', views.delete), 
 ]
--- python/django/myapp/views.py
+++ python/django/myapp/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 nextId = 4
-topics = [
+topics = [ # 리스트를 큰 저장소로 잡고 세부 내용은 딕셔너리로 묶어둠.
     {'id':1, 'title':'routing', 'body':'routing is..'},
     {'id':2, 'title':'views', 'body':'views is..'},
     {'id':3, 'title':'Model', 'body':'Model is..'},
 ]
-def HTMLTemplate(articleTag):
-    global topics
-    ol = ''
-    for topic in topics:
-        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+def HTMLTemplate(articleTag, id=None): # HTML의 탬플릿 지정해줌.
+    global topics # 함수 밖의 topics를 사용하기 위함.
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
+            <li>
+                <a href = "/update/{id}">update</a>
+            <li>
+        '''
+    ol = '' # ol 태그 초기화
+    for topic in topics: # topics의 0번 부터 topic으로 불러옴.
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>' # topic
     return f'''
     <html>
     <body>
@@ -21,7 +34,8 @@ def HTMLTemplate(articleTag):
         </ul>
         {articleTag}
         <ul>
-            <li><a href = "/create/">create page</a></li>
+            <li><a href="/create/">create page</a></li>
+            {contextUI}
         </ul>
     </body>
     </html>
@@ -43,7 +57,7 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article, id))
 
 
 @csrf_exempt
@@ -68,5 +82,42 @@ def create(request):
         url = '/read/'+str(nextId)
         nextId = nextId + 1
         return redirect(url)
-    
-    #11강 까지 함.
+
+@csrf_exempt   
+def delete(request):
+    global topics
+    if request.method == 'POST':
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics        
+        return redirect('/')
+
+@csrf_exempt   
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    "title":topic['title'],
+                    "body":topic['body']
+                    }
+        article = f'''
+            <form action = "/update/{id}/" method = "post">
+                <p><input type = "text" name = "title" placeholder = "title" value={selectedTopic["title"]}></p>
+                <p><textarea name = "body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type = "submit"></p>
+            </form>
+        '''
+        return(HttpResponse(HTMLTemplate(article, id)))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body        
+        return redirect(f'/read/{id}/')
